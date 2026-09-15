9d75e76c75a6804e9163340a36635950f1bb8985
BrandonArAf <bra.arellano@duocuc.cl>
2026-09-14 21:10:36 -0300

    feat: implementar seguridad spring security oauth2 jwt en bff

---
+++ ms-pedidos360-bff/src/main/java/cl/duoc/pedidos360/MsPedidos360BffApplication.java
@@ -0,0 +1,12 @@
+package cl.duoc.pedidos360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedidos360BffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPedidos360BffApplication.class, args);
+    }
+}
